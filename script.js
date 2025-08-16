e8d0297f137301b71035541fb8b89558eae87abc
thesidsupreme <siddharthnodiyal180@gmail.com>
2025-08-17 01:36:07 +0530

    add some js and songs

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Har Har Mahadev")
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)  
+        }
+    }
+    return songs
+
+    
+}
+async function main() {
+    let songs = await getSongs()
+    console.log(songs)
+    
+}
+main()
